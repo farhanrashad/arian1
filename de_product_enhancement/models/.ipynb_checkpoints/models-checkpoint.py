b040826015689144cc035c404f6245bbff12f9f9
dynexcel-ddc1 <ddc1@dynexce.co>
2020-11-12 12:53:39 +0500

    update receipt date

--- de_product_enhancement/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_product_enhancement/models/.ipynb_checkpoints/models-checkpoint.py
@@ -2,6 +2,16 @@
 
 from odoo import models, fields, api, _
 
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    
+    receipt_date = fields.Date(string='Receipt Date')
+    
+    @api.onchange('receipt_date')
+    def onchange_receipt_date(self):
+        if self.receipt_date:
+            self.date_planned = self.receipt_date
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
