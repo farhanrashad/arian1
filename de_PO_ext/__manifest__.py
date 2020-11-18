1ba6b3fbef1f0706d4ce9ad69aa2f2c6ce779c92
dynexcel-ddc1 <64604276+dynexcel-ddc1@users.noreply.github.com>
2020-11-18 17:31:47 +0500

    module pushed de_PO_ext

---
+++ de_PO_ext/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': " purchase order ext",
+
+    'summary': """
+       This module will make read only clumn or source field n
+       """,
+
+    'description': """
+         This module will make read only clumn or source field n
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Employee',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        'views/purchase_order_ext.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
