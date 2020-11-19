c87b08a08641af6b9ac51ce39130378cf89c6383
dynexcel-ddc1 <64604276+dynexcel-ddc1@users.noreply.github.com>
2020-11-19 15:31:49 +0500

    module pushed de_partner_fields_required

--- de_partner_fields_required/models/res_users_ext.py
+++ de_partner_fields_required/models/res_users_ext.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, models, modules,fields, _
 
 class UsersExt(models.Model):
     _inherit = 'res.partner'
 
-    region = fields.Char(string="Region",required=True)
+    region = fields.Selection([
+        ('local' , 'Local'),
+        ('foriegn','Foriegn')
+    ], default='local', string="Region" ,required=True)
 
     mobile = fields.Char(required=True)
     category_id = fields.Many2many(required=True)
     street = fields.Char(required=True)
     street2 = fields.Char(required=True)
     city = fields.Char( required=True)
-    # state_id = fields.Many2one(required=True)
-    # zip = fields.Char( required=True)
-    # country_id = fields.Many2one( required=True)
 
     property_supplier_payment_term_id = fields.Many2one( required=True)
+
