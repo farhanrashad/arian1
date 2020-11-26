e5f122c93d191b61fdd0b2eca37e1b5f6b99aa6e
dynexcel-ddc1 <64604276+dynexcel-ddc1@users.noreply.github.com>
2020-11-26 13:04:10 +0500

    de_update_vendor_dubcontractor

---
+++ de_update_vendor_subcontractor/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Order Vendor Subcontractor",
+
+    'summary': """
+       This module will add a check box fields  in form view and a security group for that """,
+
+    'description': """
+         This module will add a check box fields  in form view and a security group for that
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
+
+    'category': 'Employee',
+    'version': '13.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','purchase'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/update_vendor_subcontractor_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
