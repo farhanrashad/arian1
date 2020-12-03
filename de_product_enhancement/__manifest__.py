e1b7205b1265972fff70324eb09ad1ba25cf17c0
dynexcel-ddc1 <ddc1@dynexce.co>
2020-12-03 18:09:13 +0500

    update  stock quant

--- de_product_enhancement/__manifest__.py
+++ de_product_enhancement/__manifest__.py
@@ -20,12 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','sale','purchase','stock'],
+    'depends': ['base','product','sale','purchase','stock','mrp','account'],
 
     # always loaded
     'data': [
-         'security/security.xml',
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
