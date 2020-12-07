898480f8f9ceac59a282c4fd1d1caa77d9926745
dynexcel-ddc1 <ddc1@dynexce.co>
2020-12-07 21:36:10 +0500

    update

--- de_product_enhancement/__manifest__.py
+++ de_product_enhancement/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "de_product_enhancement",
+    'name': "Product Enhancement",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Purhcase Order Payment Term Manangement""",
 
     'description': """
-        Long description of module's purpose
+        1- Purhcase Order Payment Term Manangement
+        2-   
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
