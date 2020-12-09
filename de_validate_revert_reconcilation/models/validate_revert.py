60fe4a36b91061f4a186d4e7e8940db548756bd6
dynexcel-ddc1 <ddc1@dynexcel.co>
2020-12-09 13:28:25 +0000

    updated

--- de_validate_revert_reconcilation/models/validate_revert.py
+++ de_validate_revert_reconcilation/models/validate_revert.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models,fields,  _
+from odoo import exceptions
+from odoo.exceptions import UserError, ValidationError
 
 class AccountBandkStatementInherit(models.Model):
     _inherit = 'account.bank.statement'
@@ -22,3 +24,12 @@ class AccountBandkStatementInherit(models.Model):
         })
         
         
+#     @api.multi
+    def write(self, values):
+        if self.state != 'open':
+            t_uid = self.env.uid
+            if not self.user_has_groups('de_validate_revert_reconcilation.revert_reconciliation_users'):
+                raise exceptions.ValidationError('You are not allowed to make changes here!')
+     
+        res = super(AccountBandkStatementInherit, self).write(values)
+        return res
