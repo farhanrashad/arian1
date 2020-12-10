ae8c7d554dc1247dacd0e5d86f0b27f88a5a985f
dynexcel-ddc1 <ddc1@dynexce.co>
2020-12-10 17:28:16 +0500

    update reconcilation

--- de_validate_revert_reconcilation/models/validate_revert.py
+++ de_validate_revert_reconcilation/models/validate_revert.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from odoo import models,fields,  _
+from odoo import exceptions
+from odoo.exceptions import UserError, ValidationError
 
 class AccountBandkStatementInherit(models.Model):
     _inherit = 'account.bank.statement'
     
-
-    is_access_revert = fields.Boolean('Access Reverst')
     state = fields.Selection(selection_add=[
               ('sent_for_validation', 'Sent for Validation'),
               ('confirm', 'Validated'),
@@ -20,5 +20,22 @@ class AccountBandkStatementInherit(models.Model):
         self.write({
             'state': 'confirm',
         })
-        
-        
+                  
+               
+#     @api.multi
+    def write(self, values):
+
+            st=''
+            sql = """ select state from account_bank_statement where id ='""" + str(self.id) + """' """
+            self.env.cr.execute(sql)
+            st = self.env.cr.fetchone()
+            if str(st[0]) == 'open':
+                res = super(AccountBandkStatementInherit, self).write(values)
+                return res
+                 
+            else:  
+                
+                if not self.user_has_groups('de_validate_revert_reconcilation.revert_reconciliation_users'):
+                    raise exceptions.ValidationError('You are not allowed to make any change here!')
+                res = super(AccountBandkStatementInherit, self).write(values)
+                return res
