78ead1b5047b4b4ad9179606c3b861865bb3a80b
dynexcel-ddc1 <ddc1@dynexcel.co>
2021-01-02 13:31:07 +0000

    updated

--- de_stock_approval/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_stock_approval/models/.ipynb_checkpoints/models-checkpoint.py
@@ -40,3 +40,23 @@ class stockPickingInherit(models.Model):
 
         return res
  
+    def button_validate(self):
+        
+        origins = self.env['stock.picking'].search([('origin','=',self.name),('state','=', 'done')])
+        for picking_line in  self.move_ids_without_package:            
+            bom = self.env['mrp.bom'].search([('product_id','=',picking_line.product_id.id)])
+            for bom_line in bom.bom_line_ids:                    
+                for document in origins:
+                    for doc_line in document.move_ids_without_package:
+                        if doc_line.product_id.id == bom_line.product_id.id:
+                            if picking_line.product_uom_qty <= doc_line.quantity_done: 
+                                pass
+                            else:
+                                raise UserError(('You can only reserved quantity '+ doc_line.quantity_done + ' for product ' + picking_line.product_id.name))
+
+        
+        res = super(stockPickingInherit, self).button_validate()
+        
+        return res
+
+
