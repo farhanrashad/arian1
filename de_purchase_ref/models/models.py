5cdfd465335cda52f37eef25cc341f7d4a05cd78
dynexcel-ddc1 <ddc1@dynexcel.co>
2021-01-12 01:28:55 +0000

    updated

--- de_purchase_ref/models/models.py
+++ de_purchase_ref/models/models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-
+from datetime import date, datetime, timedelta
+import dateutil.parser
+from odoo.exceptions import UserError
 
 class StockMoveInherit(models.Model):
     _inherit = 'stock.move.line'
@@ -29,4 +31,24 @@ class MrpProductionInherit(models.Model):
     def gettingd_category_id(self):
         self.category_id = self.categ_id
         
-        
+class SaleOrderInherit(models.Model):
+    _inherit = 'sale.order'
+    
+    def calculate_date(self):    
+        specific_date = self.commitment_date
+        new_due_date = specific_date+ timedelta(days = self.insert_days) 
+        new_date = datetime.strptime(str(new_due_date),'%Y-%m-%d %H:%M:%S')
+        new_date = new_date.date()
+        new_date=str(new_date)
+#         raise UserError((type(self.bl_date)))
+        self.bl_date = new_date
+                
+    insert_days = fields.Integer(string="Expected On Boarding Date after Delivery Date")
+    days_days = fields.Char(default=' ',readonly=True)
+    
+    bl_date = fields.Char(string="BL Date", readonly=True, compute='calculate_date')
+    
+    
+
+
+#  compute='calculate_date'
