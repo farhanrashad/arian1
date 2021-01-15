e007fd9414ff2fa554ead61d4a7db56dbb20db8a
NSKTHR <60393666+NSKTHR@users.noreply.github.com>
2021-01-15 12:08:41 +0500

    BL date Module

---
+++ de_purchase_ref/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Menufacturing Pivot Ref",
+
+    'summary': """
+        this module is about to add reference in pivot view and filter dropdown
+        """,
+
+    'description': """
+        this module is about to add reference in pivot view and filter dropdown
+    """,
+
+    'author': "Dynexcel",
+    'website': "http://www.dynexcel.co",
+
+
+    'category': 'purchase',
+    'version': '13.0.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','mrp','stock','sale','account_accountant'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+#         'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
