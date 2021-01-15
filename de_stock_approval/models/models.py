ffa04e89412791a9a162d314d8691b57ca4e880a
dynexcel-ddc1 <ddc1@dynexcel.co>
2021-01-15 13:22:48 +0000

    update stock

--- de_stock_approval/models/models.py
+++ de_stock_approval/models/models.py
@@ -41,20 +41,24 @@ class stockPickingInherit(models.Model):
                
         return res
  
-    def button_validate(self):        
-        origins = self.env['stock.picking'].search([('origin','=',self.name),('state','=', 'done')])
-        for picking_line in  self.move_ids_without_package:            
-            bom = self.env['mrp.bom'].search([('product_id','=',picking_line.product_id.id)])
-            for bom_line in bom.bom_line_ids:                    
-                for document in origins:
-                    for doc_line in document.move_ids_without_package:
-                        if doc_line.product_id.id == bom_line.product_id.id:
-                            if picking_line.product_uom_qty <= doc_line.quantity_done: 
-                                pass
-                            else:
-                                raise UserError(('You can only reserved quantity '+ doc_line.quantity_done + ' for product ' + picking_line.product_id.name))
+    def button_validate(self): 
+#         raise UserError(('You can only reserved quantity '))
+        if self.picking_type_id.id == 16:
+            origins = self.env['stock.picking'].search([('origin','=',self.name),('state','=', 'done')])
+            if origins:
+                for picking_line in  self.move_ids_without_package:            
+                    bom = self.env['mrp.bom'].search([('product_id','=',picking_line.product_id.id)])
+                    for bom_line in bom.bom_line_ids:                    
+                        for document in origins:
+                            for doc_line in document.move_ids_without_package:
+                                if doc_line.product_id.id == bom_line.product_id.id:
+                                    if picking_line.product_uom_qty <= doc_line.quantity_done: 
+                                        pass
+                                    else:
+                                        raise UserError(('You can only reserved quantity '+ doc_line.quantity_done + ' for product ' + picking_line.product_id.name))
+            else:                            
+                raise UserError(('Please First Validate it Delivery !'))    
 
-        
         res = super(stockPickingInherit, self).button_validate()
         
         return res
