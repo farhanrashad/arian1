31adcdd548389fb847606b20249890bf220be9d7
dynexcel-ddc1 <64604276+dynexcel-ddc1@users.noreply.github.com>
2021-02-01 11:40:22 +0500

    module pushed de_sale_order by Musadiq

--- de_sale_order/__manifest__.py
+++ de_sale_order/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Sale Order product parent type",
+    'name': "Sale Order Product Parent Type",
 
     'summary': """
         this module is about to make total of products according to their parent type
@@ -11,7 +11,7 @@
     """,
 
     'author': "Dynexcel",
-    'website': "http://www.dynexcel.com",
+    'website': "http://www.dynexcel.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -25,10 +25,6 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/sale_order_view.xml',
     ],
 }
