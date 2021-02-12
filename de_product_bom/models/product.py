292831edd04761ad6d88045f76923b8fb56c15ea
NSKTHR <60393666+NSKTHR@users.noreply.github.com>
2021-02-12 17:57:21 +0500

    de_product_bom by Khalid

---
+++ de_product_bom/models/product.py
@@ -0,0 +1,34 @@
+from odoo import fields, models
+from odoo.exceptions import UserError
+
+
+class ProductBom(models.Model):
+    _inherit = 'product.template'
+
+
+    def action_craete_bom(self):
+        variants = self.env['product.product'].search([('product_tmpl_id','=',self.id)])
+        bom_vals = []
+        bom_exists = self.env['mrp.bom'].search([('product_tmpl_id', '=', self.id)])
+
+        if not bom_exists:
+            raise UserError("Please create atleast one bom:")
+        for bom in variants:
+            component_lines = []
+            for line in bom_exists.bom_line_ids:
+                component_lines.append((0,0,{
+                    'product_id': line.product_id.id,
+                    'product_qty': line.product_qty,
+                }))
+            variants_bom_exists = self.env['mrp.bom'].search([('product_tmpl_id', '=', self.id),('product_id','=',bom.id)])
+            if not variants_bom_exists:
+                vals = {
+                        'product_tmpl_id': self.id,
+                        'product_id': bom.id,
+                        'product_qty':  1,
+                        'type': 'normal',
+                        'bom_line_ids': component_lines,
+                        }
+                generated_bom = self.env['mrp.bom'].create(vals)
+            # raise UserError('BOM CREATED')
+
