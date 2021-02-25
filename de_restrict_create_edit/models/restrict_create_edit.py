7e151952b82cebc8ec604003bbe52466f498912f
NSKTHR <60393666+NSKTHR@users.noreply.github.com>
2021-02-25 10:36:03 +0500

    de_restrict_create_edi

--- de_restrict_create_edit/models/restrict_create_edit.py
+++ de_restrict_create_edit/models/restrict_create_edit.py
@@ -16,6 +16,6 @@ class StockPickingInh(models.Model):
 
     def write(self, values):
         if not self.user_has_groups('de_restrict_create_edit.group_edit_access'):
-            pass
-        else:
             raise exceptions.ValidationError('Group is not allowed!')
+        else:
+            return super(StockPickingInh, self).write(values)
