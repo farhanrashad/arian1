8cfd421356227db4dd011cf26c23cf2e63630ff6
Fawaad Suleman <fawaad.s@dynexcel.co>
2021-02-26 12:04:17 +0500

    de_increment

--- de_increment/__manifest__.py
+++ de_increment/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '14.0.0.0',
     "category": 'Increment',
     "summary": ' Employee Increment',
-    'sequence': -10,
+    'sequence': 0,
     "description": """" De_Increment is an module that inherit with Employee
      module. The purpose of this module add one more tab "Incremant" 
        where it have a filed about wage increment and save all the data inside into it. 
@@ -18,9 +18,10 @@
     'depends': ['base', 'hr','hr_contract'],
     'data': [
         'data/increment_action.xml',
+        'report/increment_report.xml',
         'security/ir.model.access.csv',
         'views/de_employee_increment.xml',
-        'report/increment_report.xml',
+
 
     ],
 
--- de_increment/models/de_employee_increment.py
+++ de_increment/models/de_employee_increment.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from odoo.exceptions import UserError
 from datetime import date
 
@@ -9,27 +9,17 @@ class EmployeeTab(models.Model):
     increment_id = fields.One2many('hr.contract.increment', 'contract_id', string='increment')
 
     def employee_increment_action(self):
-        print('auto-increment-call-------------  ')
-        current_date_time = date.today()
-        contract_ids = self.env['hr.contract'].search([])
+        current_date = date.today()
+        contract_ids = self.env['hr.contract'].search([('state', '=', 'open')])
         for contract in contract_ids:
-            print('--------- contract name', contract.name)
-
-            for rec in contract_ids.increment_id:
-
-                inc_amount = rec.increment_amount
-                print('incremnt info-------', inc_amount)
-                eft_date = rec.increment_effective_date
-                print('effective date-------', eft_date)
-
-                if current_date_time == eft_date:
-                    total_wage = self.wage + inc_amount
-                    # print('wages info-------', self.wage)
-
-                    print('total of wages-------', total_wage)
-
-                    # self.wage = total_wage
-                    # contract.write({'wage':total_wage})
+            if contract.increment_id:
+                for rec in contract.increment_id:
+                    if rec.is_increment_applied == False:
+                        if rec.increment_effective_date == current_date:
+                            total_wage = contract.wage + rec.increment_amount
+                            contract.wage = total_wage
+                            rec.is_increment_applied = True
+                            print('total of wages-------', contract.wage)
 
 
 class EmployeeIncrement(models.Model):
@@ -38,18 +28,26 @@ class EmployeeIncrement(models.Model):
 
     years = fields.Selection(selection=
                              [('2021', '2021'), ('2022', '2022'), ('2023', '2023'),
-                              ('2024', '2024'), ('2025', '2025')], string='Years', required=True)
+                              ('2024', '2024'), ('2025', '2025')], string='Years')
     increment_amount = fields.Float(string='Amount', required=True)
     increment_effective_date = fields.Date(string='Increment Effective Date', required=True)
     contract_id = fields.Many2one('hr.contract')
-
-    # @api.model
-    # def create(self, vals):
-    #     # if vals.get('name', _('New')) == _('New'):
-    #     #     vals['name'] = self.env['ir.sequence'].next_by_code('employee.insurance.sequence')
-    #
-    #     if int(vals['increment_amount']) <= 0:
-    #         raise UserError('Insurance amount must be greater than 0.')
-    #
-    #     result = super(EmployeeIncrement, self).create(vals)
-    #     return result
+    is_increment_applied = fields.Boolean(default=False, readonly=True)
+    
+    
+    def unlink(self):
+        if self.is_increment_applied == True:
+            raise UserError(('Deletion is not allowed, Incase of increment is applied!'))
+        return super(EmployeeIncrement,self).unlink()
+    
+    
+    @api.constrains('increment_amount')
+    def check_amount(self):
+        if self.increment_amount <= 0:
+            raise UserError(('Increment amount must be greater than 0.'))
+    
+    
+    
+    
+    
+    
