8eea7b77ef9322c874a32ad38db332d279f5a42e
NSKTHR <60393666+NSKTHR@users.noreply.github.com>
2021-04-19 13:18:49 +0500

    de_partner_fields

---
+++ de_partner_fields_required/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contacts fields",
+
+    'summary': """
+           Contacts fields
+           """,
+
+    'description': """
+           Contacts fields
+    """,
+
+    'author': "Dynexcel",
+    'website': "https://www.dynexcel.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Base',
+    'version': '13.0.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'views/contact_views_ext.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
