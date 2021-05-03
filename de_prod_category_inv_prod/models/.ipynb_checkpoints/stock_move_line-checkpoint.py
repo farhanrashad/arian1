153862cc913f07bfda1b34692aa482511373e8fc
zeeshanDynexcels <zeeshan.r@dynexcel.co>
2021-05-03 07:18:34 +0000

    fix inv prod

--- de_prod_category_inv_prod/models/.ipynb_checkpoints/stock_move_line-checkpoint.py
+++ de_prod_category_inv_prod/models/.ipynb_checkpoints/stock_move_line-checkpoint.py
@@ -5,13 +5,13 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
     _description = ''
     
-    contact = fields.Char('Contact', compute='_compute_contact')
+    contact = fields.Many2one('res.partner', compute='_compute_contact')
     
     
     def _compute_contact(self):
         for rec in self:
             stock_picking_obj = self.env['stock.picking'].search([('name', '=', rec.reference)])
             if stock_picking_obj:
-                rec.contact = stock_picking_obj[0].name
+                rec.contact = stock_picking_obj[0].partner_id.id
             else:
                 rec.contact = None
