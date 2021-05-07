b39de17fefbff5841c38e1acb1a985ebee371008
NSKTHR <60393666+NSKTHR@users.noreply.github.com>
2021-05-07 12:06:33 +0500

    de_prod_category

---
+++ de_prod_category_inv_prod/models/.ipynb_checkpoints/stock_move_line-checkpoint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+    _description = ''
+    
+    contact = fields.Many2one('res.partner', compute='_compute_contact', store=True)
+    
+    
+    def _compute_contact(self):
+        for rec in self:
+            stock_picking_obj = self.env['stock.picking'].search([('name', '=', rec.reference)])
+            if stock_picking_obj:
+                rec.contact = stock_picking_obj[0].partner_id.id
+            else:
+                rec.contact = None
