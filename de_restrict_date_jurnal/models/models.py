0ea6d7f8a59bbd81c7b056e5059ecf3407feb3ba
dynexcel-ddc1 <ddc1@dynexce.co>
2021-08-09 12:54:54 +0500

    update restrict date jurnal

--- de_restrict_date_jurnal/models/models.py
+++ de_restrict_date_jurnal/models/models.py
@@ -21,18 +21,17 @@ class AccountMoveInherit(models.Model):
                         if self.invoice_date < past_date:
                             raise UserError("Sorry, you are not allowed to post Bills/Invoices/JVs in dates prior to " + str(past_date) + "!")
 
-    
     @api.constrains('date')
-    def future_invoice_date_restriction(self):  
-        today = date.today()
-        if self.date:
-            if self.date > today:
-                raise UserError("Sorry! You can't select future Accounting Date!")
-            else:
-                if not self.user_has_groups('de_restrict_date_jurnal.group_back_date_allow_users'): 
-                    past_date = today - timedelta(days = 3)
-                    if self.date < past_date:
-                        raise UserError("Sorry, you are not allowed to post Bills/Invoices/JVs in dates prior to " + str(past_date))
+    def future_invoice_date_restriction(self): 
+        for line in self:
+            today = date.today()
+            if line.date:
+                if line.date > today:
+                    raise UserError("Sorry! You can't select future Accounting Date!")
+                else:
+                    if not line.user_has_groups('de_restrict_date_jurnal.group_back_date_allow_users'): 
+                        past_date = today - timedelta(days = 3)
+                        if line.date < past_date:
+                            raise UserError("Sorry, you are not allowed to post Bills/Invoices/JVs in dates prior to " + str(past_date))
                   
                     
-   
