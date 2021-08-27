b54c755159b8c0b29c656f62eae5ab376889cc06
NSKTHR <60393666+NSKTHR@users.noreply.github.com>
2021-08-27 15:30:26 +0500

    de_source_ref_je by Salman

---
+++ de_source_ref_je/models/account_move.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from collections import defaultdict
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.tools import float_compare, float_round, float_is_zero
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+    
+    source_ref = fields.Char(string='Source Ref')
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+
+    def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id, qty, description, svl_id, cost):
+        self.ensure_one()
+        AccountMove = self.env['account.move'].with_context(default_journal_id=journal_id)
+
+        move_lines = self._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id, description)
+        if move_lines:
+            date = self._context.get('force_period_date', fields.Date.context_today(self))
+            new_account_move = AccountMove.sudo().create({
+                'journal_id': journal_id,
+                'line_ids': move_lines,
+                'date': date,
+                'ref': description,
+                'source_ref': self.picking_id.origin,
+                
+                'stock_move_id': self.id,
+                'stock_valuation_layer_ids': [(6, None, [svl_id])],
+                'type': 'entry',
+            })
+            new_account_move.post()
